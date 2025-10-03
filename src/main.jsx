85c8a9f3b86ddd6b6d4fce52aae1e44d5ab77932
Mayank <mayanksharma07123@gmail.com>
2025-10-04 03:44:03 +0530

    Initial commit

---
+++ src/main.jsx
@@ -0,0 +1,31 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './index.css';
+import UIkit from 'uikit';
+import Icons from 'uikit/dist/js/uikit-icons';
+import { Provider } from 'react-redux';
+import { ToastProvider } from 'react-toast-notifications';
+import App from './App';
+import * as serviceWorker from './serviceWorker';
+import init from './init';
+import store from './store';
+
+init().then(() => {
+  // Load UIkit Icons plugin
+  UIkit.use(Icons);
+
+  ReactDOM.render(
+    <React.StrictMode>
+      <Provider store={store}>
+        <ToastProvider autoDismiss autoDismissTimeout={6000} placement="top-right">
+          <App />
+        </ToastProvider>
+      </Provider>
+    </React.StrictMode>,
+    document.getElementById('root')
+  );
+
+  // If you want your app to work offline and load faster, you can change
+  // unregister() to register() below. Note this comes with some pitfalls.
+  serviceWorker.unregister();
+});
